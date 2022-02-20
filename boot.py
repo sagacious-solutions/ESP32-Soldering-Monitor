9879d7b7a471aead7183241c39c92d94087ca0cc
Matt Macdonald <mksmacdonald@gmail.com>
2022-02-19 16:53:24 -0800

    initializing, but error. Adding pull up resistor

--- boot.py
+++ boot.py
@@ -1,8 +1,10 @@
 from machine import Pin, I2C
 import lcd.python_lcd as python_lcd
+import mlx90614
 import time
 
 LCD_ADDRESS_FROM_SCAN = 39
+TEMP_SENSOR_I2C_ADDR = 90
 
 i2c = I2C(1, scl=Pin(22), sda=Pin(21), freq=400000)
 
@@ -13,7 +15,14 @@ LCD_DISPLAY = python_lcd.I2cLcd(
     i2c=i2c, i2c_addr=LCD_ADDRESS_FROM_SCAN, num_lines=4, num_columns=20
 )
 
+
 for i in range(100):
+    try:
+        TEMP_SENSOR = mlx90614.MLX90614(i2c, TEMP_SENSOR_I2C_ADDR)
+    except Exception as e:
+        print(e)
+        pass
+
     LCD_DISPLAY.clear()
     LCD_DISPLAY.putstr(f"Hello World : {i}")
     time.sleep(1)
